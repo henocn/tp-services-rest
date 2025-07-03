7feeaa53c56068c9eaf06702e3557f06ada2a81a
Henoc N'GASAMA <ngasamah@gmail.com>
2025-07-03 16:26:34 +0000

    feat: model of product

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
-    
-})
+const henocProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['Electronics', 'Clothing', 'Books', 'Fournitures', 'Others']
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+
+}, { timestamps: true });
