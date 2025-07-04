57728af961f9152df0a438455d04ebce5cacecd0
Henoc N'GASAMA <ngasamah@gmail.com>
2025-07-04 11:21:57 +0000

    feat: password validation

--- models/User.js
+++ models/User.js
@@ -9,12 +9,15 @@ const henocUserSchema = new mongoose.Schema({
         match: [/^[^\s@]+@[^\s@]+\.[$\s@]+$/, "Please enter a valid email"]
     },
     password: {
-        type: Number,
+        type: String,
         required: true,
+        minlength : [8, 'Password should be at least 8 characters long'],
+        maxlength : [10, 'Password should be at least 8 characters long'],
+        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,10}$/, 'Password must contain at least one uppercase letter, one lowercase letter, and one number']
     },
     role: {
         type: String,
-        enim: ['superadmin', 'admin', 'user']
+        enum: ['superadmin', 'admin', 'user']
     },
     quantity: {
         type: Number,
