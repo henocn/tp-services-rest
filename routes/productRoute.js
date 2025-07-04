6010a50c1211a5029651684f3573216e68fe6c12
Henoc N'GASAMA <ngasamah@gmail.com>
2025-07-04 09:25:59 +0000

    feat: add product routes for all endpoints

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,4 +3,8 @@ const router = express.Router();
 const actions = require("../controllers/productController")
 
 router.post('/', actions.addProduct);
+router.get('/', actions.getAllProducts);
+router.get('/:id', actions.getElementById);
+router.put('/:id', actions.updateProduct);
+// router.delete('/:id', actions.deleteProduct);
 module.exports = router
