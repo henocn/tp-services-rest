1d3a6c989f9a21747954bb06c19fb835f3cb3495
Henoc N'GASAMA <ngasamah@gmail.com>
2025-07-04 00:15:43 +0000

    routes configurations

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/database');
 require('dotenv').config();
+const productRoute = require('./routes/productRoute');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/products', productRoute);
 
 connectDB();
 
